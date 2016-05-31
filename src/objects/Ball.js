9f43a81509833b3c09362aa910a5d8709e4ce2f8
Guillaume Gouessan <guillaume.gouessan@gmail.com>
2016-05-31 22:57:30 +0200

    better color on default object

--- src/objects/Ball.js
+++ src/objects/Ball.js
@@ -5,7 +5,7 @@ export default class Ball extends THREE.Object3D {
     super();
 
     const geometry = new THREE.TorusKnotBufferGeometry(1, 0.25, 100, 16);
-    const material = new THREE.MeshStandardMaterial({color: 0xff6838, roughness: 0.18, metalness: 0.5});
+    const material = new THREE.MeshStandardMaterial({color: 0xA197C9, roughness: 0.18, metalness: 0.5});
     const mesh = new THREE.Mesh(geometry, material);
 
     this.add(mesh);
